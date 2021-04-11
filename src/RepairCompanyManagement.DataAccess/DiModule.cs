3aef23658f3cfe80538e9f01329c4a434964b481
Vadzim Zemchonak <37553515+Zemchonak@users.noreply.github.com>
2021-04-11 16:22:39 +0300

    Update DataAccess.: add NuGet Autofac package + add DI module

---
+++ src/RepairCompanyManagement.DataAccess/DiModule.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using RepairCompanyManagement.DataAccess.Repositories;
+
+namespace RepairCompanyManagement.DataAccess
+{
+    public class DiModule : Module
+    {
+        private readonly string _connectionString;
+
+        public DiModule(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            // here we should add our repositories
+            // to use them with Dependency Injection Autofac Container
+
+            builder.RegisterType<SpecializationRepository>()
+                .WithParameter("connectionString", _connectionString)
+                .AsImplementedInterfaces().InstancePerLifetimeScope();
+        }
+    }
+}
