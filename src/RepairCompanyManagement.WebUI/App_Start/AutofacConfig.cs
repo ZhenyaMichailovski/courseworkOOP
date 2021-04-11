777a3f3a4c58e9d99cbdfc6dbcbb61aa12d8888b
Vadzim Zemchonak <37553515+Zemchonak@users.noreply.github.com>
2021-04-11 20:08:17 +0300

    Update WebUI: add Autofac config

---
+++ src/RepairCompanyManagement.WebUI/App_Start/AutofacConfig.cs
@@ -0,0 +1,60 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using AutoMapper;
+using RepairCompanyManagement.BusinessLogic.Mapper;
+using RepairCompanyManagement.WebUI.Mapper;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Web.Mvc;
+
+namespace RepairCompanyManagement.WebUI.App_Start
+{
+    public class AutofacConfig
+    {
+        private const string connectionStringName = "DefaultConnect";
+
+        private AutofacConfig()
+        {
+        }
+
+        public static void Run()
+        {
+            ConfigureContainer();
+        }
+
+        public static void ConfigureContainer()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings[connectionStringName].ConnectionString;
+
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            builder.RegisterModule<AutofacWebTypesModule>();
+
+            // builder.RegisterType<UserStore<User>>().As<IUserStore<User>>().InstancePerRequest();
+            // builder.RegisterType<ApplicationUserManager>().AsSelf().InstancePerRequest();
+            // builder.RegisterType<ApplicationSignInManager>().AsSelf().InstancePerRequest();
+            // builder.Register<IAuthenticationManager>(c => HttpContext.Current.GetOwinContext().Authentication).InstancePerRequest();
+
+            builder.RegisterModule(new RepairCompanyManagement.BusinessLogic.DiModule(connectionString));
+
+            builder.Register(ctx => new MapperConfiguration(cfg =>
+                cfg.AddProfiles(new List<Profile>
+                    {
+                        new BusinessLogicMapperProfile(),
+                        new WebUIMapperProfile(),
+                    })))
+                .AsSelf().SingleInstance();
+            builder.Register(c =>
+            {
+                var context = c.Resolve<IComponentContext>();
+                var config = context.Resolve<MapperConfiguration>();
+                return config.CreateMapper(context.Resolve);
+            })
+            .As<IMapper>()
+            .InstancePerLifetimeScope();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
--- src/RepairCompanyManagement.WebUI/Global.asax.cs
+++ src/RepairCompanyManagement.WebUI/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using RepairCompanyManagement.WebUI.App_Start;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
@@ -12,6 +13,7 @@ namespace RepairCompanyManagement.WebUI
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            AutofacConfig.Run();
         }
     }
 }
