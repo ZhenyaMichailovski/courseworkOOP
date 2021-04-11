8c846990b9e2a25c52830d07dda0751c39033edc
Vadzim Zemchonak <37553515+Zemchonak@users.noreply.github.com>
2021-04-11 16:59:19 +0300

    Update BusinessLogic: add Constants class to store constants & messages

---
+++ src/RepairCompanyManagement.BusinessLogic/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace RepairCompanyManagement.BusinessLogic
+{
+    public class Constants
+    {
+        public const string EmptySpecializationNameMessage = "Specialization should have a not empty name!";
+
+        public const string SpecializationNotFoundMessage = "The specific specialization wasn't found!";
+    }
+}
