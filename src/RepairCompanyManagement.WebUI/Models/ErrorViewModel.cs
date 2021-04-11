0305cf11dd82d32fb29ec8e796d7ffdb337027ca
Vadzim Zemchonak <37553515+Zemchonak@users.noreply.github.com>
2021-04-11 20:08:51 +0300

    Update WebUI: add Error model & view

---
+++ src/RepairCompanyManagement.WebUI/Models/ErrorViewModel.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace RepairCompanyManagement.WebUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string ErrorMessage { get; set; }
+
+        public string RedirectionControllerName { get; set; }
+
+        public string RedirectionActionName { get; set; }
+
+        public object RedirectionRouteValues { get; set; }
+    }
+}
