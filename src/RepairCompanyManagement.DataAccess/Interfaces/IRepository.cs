1dfaf6a444074de9fdc7599cd2cb6921d1dbfe37
Vadzim Zemchonak <37553515+Zemchonak@users.noreply.github.com>
2021-04-11 16:14:37 +0300

    Update DataAccess. Add IRepository interface

---
+++ src/RepairCompanyManagement.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace RepairCompanyManagement.DataAccess.Interfaces
+{
+    public interface IRepository<T> 
+        where T : class
+    {
+        int Create(T item);
+        
+        T GetById(int id);
+        
+        IEnumerable<T> GetAll();
+        
+        void Update(T item);
+        
+        void Delete(int id);
+    }
+}
