58a7d643ed4ec66fc288fbcdbc8270a86207e36e
Vadzim Zemchonak <37553515+Zemchonak@users.noreply.github.com>
2021-04-11 20:08:35 +0300

    Update WebUI:  add Automapper profile

---
+++ src/RepairCompanyManagement.WebUI/Mapper/WebUIMapperProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using RepairCompanyManagement.BusinessLogic.Dtos;
+using RepairCompanyManagement.WebUI.Models;
+
+namespace RepairCompanyManagement.WebUI.Mapper
+{
+    public class WebUIMapperProfile : Profile
+    {
+        public WebUIMapperProfile()
+        {
+            CreateMap<SpecializationDto, SpecializationViewModel>().ReverseMap();
+        }
+    }
+}
