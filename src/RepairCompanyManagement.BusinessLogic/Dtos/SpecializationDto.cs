34e3d6783a8b16bccd9c06a6b0365ecbd8d99add
Vadzim Zemchonak <37553515+Zemchonak@users.noreply.github.com>
2021-04-11 16:58:13 +0300

    Update BusinessLogic: add Specialization DTO

---
+++ src/RepairCompanyManagement.BusinessLogic/Dtos/SpecializationDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RepairCompanyManagement.BusinessLogic.Dtos
+{
+    public class SpecializationDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+    }
+}
