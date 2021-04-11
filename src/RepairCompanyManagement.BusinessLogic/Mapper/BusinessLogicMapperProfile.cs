0d863c8d342c1576af988a6d235f0b7c1392d805
Vadzim Zemchonak <37553515+Zemchonak@users.noreply.github.com>
2021-04-11 16:58:35 +0300

    Update BusinessLogic: add AutoMapper rule for Specialization

---
+++ src/RepairCompanyManagement.BusinessLogic/Mapper/BusinessLogicMapperProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using RepairCompanyManagement.BusinessLogic.Dtos;
+using RepairCompanyManagement.DataAccess.Entities;
+
+namespace RepairCompanyManagement.BusinessLogic.Mapper
+{
+    class BusinessLogicMapperProfile : Profile
+    {
+        public BusinessLogicMapperProfile()
+        {
+            // here we should have all the rules for mapping
+            // between DTOs and simple entities
+
+            CreateMap<Specialization, SpecializationDto>().ReverseMap();
+        }
+    }
+}
