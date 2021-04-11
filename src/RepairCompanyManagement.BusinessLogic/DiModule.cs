3b940dc874b95dca3508e1be6a733080f8ae089d
Vadzim Zemchonak <37553515+Zemchonak@users.noreply.github.com>
2021-04-11 16:59:36 +0300

    Update BusinessLogic: add Autofac DI container module

---
+++ src/RepairCompanyManagement.BusinessLogic/DiModule.cs
@@ -0,0 +1,26 @@
+﻿using Autofac;
+using RepairCompanyManagement.BusinessLogic.Services;
+
+namespace RepairCompanyManagement.BusinessLogic
+{
+    class DiModule : Module
+    {
+        private readonly string _connectionString;
+
+        public DiModule(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule(new RepairCompanyManagement.DataAccess.DiModule(_connectionString));
+
+            // here we should add our BusinessLogic services
+            // to manipulate on repositories
+
+            builder.RegisterType<BrigadeService>()
+                .AsImplementedInterfaces().InstancePerLifetimeScope();
+        }
+    }
+}
