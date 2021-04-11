4d1a69f69209b90d290fbb7aed5ecaabe9ad594d
Vadzim Zemchonak <37553515+Zemchonak@users.noreply.github.com>
2021-04-11 16:58:56 +0300

    Update BusinessLogic: create BrigadeService interface & implementation

---
+++ src/RepairCompanyManagement.BusinessLogic/Interfaces/IBrigadeService.cs
@@ -0,0 +1,53 @@
+﻿using RepairCompanyManagement.BusinessLogic.Dtos;
+using RepairCompanyManagement.DataAccess.Entities;
+using System.Collections.Generic;
+
+namespace RepairCompanyManagement.BusinessLogic.Interfaces
+{
+    public interface IBrigadeService
+    {
+        int CreateSpecialization(SpecializationDto item);
+
+        SpecializationDto GetSpecializationById(int id);
+
+        IReadOnlyCollection<SpecializationDto> GetAllSpecializations();
+
+        void UpdateSpecialization(SpecializationDto item);
+
+        void DeleteSpecialization(int id);
+
+        void ValidateSpecialization(SpecializationDto item);
+
+        //int CreateBrigade(BrigadeDto item);
+
+        //BrigadeDto GetBrigadeById(int id);
+
+        //IReadOnlyCollection<BrigadeDto> GetAllBrigades();
+
+        //void UpdateBrigade(BrigadeDto item);
+
+        //void DeleteBrigade(int id);
+
+
+        //int CreateEmployee(EmployeeDto item);
+
+        //EmployeeDto GetEmployeeById(int id);
+
+        //IReadOnlyCollection<EmployeeDto> GetAllEmployees();
+
+        //void UpdateEmployee(EmployeeDto item);
+
+        //void DeleteEmployee(int id);
+
+
+        //int CreateJobPosition(JobPositionDto item);
+
+        //JobPositionDto GetJobPositionById(int id);
+
+        //IReadOnlyCollection<JobPositionDto> GetAllJobPositions();
+
+        //void UpdateJobPosition(JobPositionDto item);
+
+        //void DeleteJobPosition(int id);
+    }
+}
---
+++ src/RepairCompanyManagement.BusinessLogic/Services/BrigadeService.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using RepairCompanyManagement.BusinessLogic.Dtos;
+using RepairCompanyManagement.BusinessLogic.Exceptions;
+using RepairCompanyManagement.BusinessLogic.Interfaces;
+using RepairCompanyManagement.DataAccess.Entities;
+using RepairCompanyManagement.DataAccess.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RepairCompanyManagement.BusinessLogic.Services
+{
+    public class BrigadeService : IBrigadeService
+    {
+        private readonly IRepository<Specialization> _specializationRepository;
+        // private readonly IRepository<Brigade> _brigadeRepository;
+        // private readonly IRepository<Employee> _employeeRepository;
+        // private readonly IRepository<JobPosition> _jobPositionRepository;
+
+        private readonly IMapper _mapper;
+
+        public BrigadeService(IRepository<Specialization> specializationRepository, IMapper mapper)
+        {
+            _specializationRepository = specializationRepository;
+            _mapper = mapper;
+        }
+
+        public int CreateSpecialization(SpecializationDto item)
+        {
+            ValidateSpecialization(item);
+
+            return _specializationRepository.Create(_mapper.Map<Specialization>(item));
+        }
+
+        public void DeleteSpecialization(int id)
+        {
+            if (_specializationRepository.GetById(id) is null)
+                throw new BusinessLogicException(Constants.SpecializationNotFoundMessage);
+
+            _specializationRepository.Delete(id);
+        }
+
+        public IReadOnlyCollection<SpecializationDto> GetAllSpecializations()
+        {
+            return _mapper.Map<IEnumerable<SpecializationDto>>(_specializationRepository.GetAll())
+                .ToList().AsReadOnly();
+        }
+
+        public SpecializationDto GetSpecializationById(int id)
+        {
+            var response = _specializationRepository.GetById(id);
+
+            if (response is null)
+            {
+                throw new BusinessLogicException(Constants.SpecializationNotFoundMessage);
+            }
+
+            return _mapper.Map<SpecializationDto>(response);
+        }
+
+        public void UpdateSpecialization(SpecializationDto item)
+        {
+            ValidateSpecialization(item);
+
+            _specializationRepository.Update(_mapper.Map<Specialization>(item));
+        }
+
+        public void ValidateSpecialization(SpecializationDto item)
+        {
+            if (string.IsNullOrEmpty(item.Name))
+                throw new ValidationException(Constants.EmptySpecializationNameMessage);
+        }
+    }
+}
