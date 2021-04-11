8d72f7bc2dffb5644b9657556004ab976fe1cd59
Vadzim Zemchonak <37553515+Zemchonak@users.noreply.github.com>
2021-04-11 16:57:55 +0300

    Update BusinessLogic: add custom exceptions
    BusinessLogic & Validation exceptions

---
+++ src/RepairCompanyManagement.BusinessLogic/Exceptions/BusinessLogicException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace RepairCompanyManagement.BusinessLogic.Exceptions
+{
+    [Serializable]
+    public class BusinessLogicException : Exception
+    {
+        public BusinessLogicException(string message)
+            : base(message)
+        {
+        }
+
+        public BusinessLogicException(string message, Exception innerException)
+        : base(message, innerException)
+        {
+        }
+
+        public BusinessLogicException()
+        {
+        }
+
+        protected BusinessLogicException(SerializationInfo serializationInfo, StreamingContext streamingContext)
+            : base(serializationInfo, streamingContext)
+        {
+        }
+    }
+}
---
+++ src/RepairCompanyManagement.BusinessLogic/Exceptions/ValidationException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace RepairCompanyManagement.BusinessLogic.Exceptions
+{
+    [Serializable]
+    public class ValidationException : Exception
+    {
+        public ValidationException(string message)
+            : base(message)
+        {
+        }
+
+        public ValidationException(string message, Exception innerException)
+        : base(message, innerException)
+        {
+        }
+
+        public ValidationException()
+        {
+        }
+
+        protected ValidationException(SerializationInfo serializationInfo, StreamingContext streamingContext)
+            : base(serializationInfo, streamingContext)
+        {
+        }
+    }
+}
