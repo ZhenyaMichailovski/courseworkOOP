a4ed98520af1475595ee5f062c105614163a5e99
Vadzim Zemchonak <37553515+Zemchonak@users.noreply.github.com>
2021-04-11 20:10:44 +0300

    Update WebUI: add Specializations Models, Views & Controller
    also added required partials

---
+++ src/RepairCompanyManagement.WebUI/Controllers/SpecializationsController.cs
@@ -0,0 +1,94 @@
+﻿using AutoMapper;
+using RepairCompanyManagement.BusinessLogic.Dtos;
+using RepairCompanyManagement.BusinessLogic.Interfaces;
+using RepairCompanyManagement.WebUI.Filters;
+using RepairCompanyManagement.WebUI.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace RepairCompanyManagement.WebUI.Controllers
+{
+    public class SpecializationsController : Controller
+    {
+        private IBrigadeService _brigadeService { get; set; }
+
+        private IMapper _mapper { get; set; }
+
+        public SpecializationsController(IBrigadeService brigadeService, IMapper mapper)
+        {
+            _brigadeService = brigadeService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ExceptionFilter()]
+        public ActionResult Index()
+        {
+            var specs = _mapper.Map<IReadOnlyCollection<SpecializationViewModel>>(_brigadeService.GetAllSpecializations());
+            return View(specs);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ExceptionFilter("Create")]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(SpecializationViewModel model)
+        {
+            if (model != null && ModelState.IsValid)
+            {
+                _brigadeService.CreateSpecialization(_mapper.Map<SpecializationDto>(model));
+
+                return RedirectToAction("Index", "Specializations", null);
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [ExceptionFilter("Index")]
+        public ActionResult Update(int id)
+        {
+            var model = _mapper.Map<SpecializationViewModel>(_brigadeService.GetSpecializationById(id));
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Update(SpecializationViewModel model)
+        {
+            if (model != null && ModelState.IsValid)
+            {
+                _brigadeService.UpdateSpecialization(_mapper.Map<SpecializationDto>(model));
+
+                return RedirectToAction("Index", "Specializations", null);
+            }
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [ExceptionFilter("Index")]
+        public ActionResult Delete(int id)
+        {
+            var model = _mapper.Map<SpecializationViewModel>(_brigadeService.GetSpecializationById(id));
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [ExceptionFilter("Index")]
+        public ActionResult ConfirmDelete(int id)
+        {
+            var model = _mapper.Map<SpecializationViewModel>(_brigadeService.GetSpecializationById(id));
+            _brigadeService.DeleteSpecialization(id);
+            
+            return RedirectToAction("Index", "Specializations", null);
+        }
+    }
+}
---
+++ src/RepairCompanyManagement.WebUI/Models/SpecializationViewModel.cs
@@ -0,0 +1,17 @@
+﻿using RepairCompanyManagement.BusinessLogic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RepairCompanyManagement.WebUI.Models
+{
+    public class SpecializationViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = Constants.EmptySpecializationNameMessage)]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+    }
+}
