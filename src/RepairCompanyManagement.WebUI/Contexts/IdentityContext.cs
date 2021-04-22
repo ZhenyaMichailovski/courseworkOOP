0133b7c60683f63f2449a50acfb4857b3265fe0e
Vadzim Zemchonak <37553515+Zemchonak@users.noreply.github.com>
2021-04-22 19:24:32 +0300

    Add Identity context

---
+++ src/RepairCompanyManagement.WebUI/Contexts/IdentityContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using RepairCompanyManagement.WebUI.Identity;
+
+namespace RepairCompanyManagement.WebUI.Contexts
+{
+    public class IdentityContext : IdentityDbContext<User>
+    {
+        public IdentityContext(string connectionString)
+            : base(connectionString)
+        {
+            Identity = Guid.NewGuid();
+        }
+
+        public Guid Identity { get; }
+    }
+}
