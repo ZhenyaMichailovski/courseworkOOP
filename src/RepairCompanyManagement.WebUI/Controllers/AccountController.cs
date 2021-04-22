22d1c76f996a42c3e679192c4b8c1059266bcdfe
Vadzim Zemchonak <37553515+Zemchonak@users.noreply.github.com>
2021-04-22 19:25:57 +0300

    Create IdentityBaseController & AccountController

---
+++ src/RepairCompanyManagement.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,167 @@
+﻿using System.Threading.Tasks;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using RepairCompanyManagement.BusinessLogic.Interfaces;
+using RepairCompanyManagement.WebUI.Identity;
+using RepairCompanyManagement.WebUI.Models;
+using TicketManagement.WebUI.Identity;
+
+namespace RepairCompanyManagement.WebUI.Controllers
+{
+    public class AccountController : IdentityBaseController
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public AccountController()
+            : base()
+        {
+        }
+
+        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+            : base(userManager, signInManager)
+        {
+        }
+
+        // GET: /Account/Login
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login(string returnPath)
+        {
+            ViewBag.ReturnPath = returnPath;
+            return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model, string returnPath)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (model != null)
+            {
+                // This doesn't count login failures towards account lockout
+                // To enable password failures to trigger account lockout, change to shouldLockout: true
+                var result = SignInManager.PasswordSignIn(model.Email, model.Password, false, shouldLockout: false);
+                switch (result)
+                {
+                    case SignInStatus.Success:
+                        return RedirectToLocal(returnPath);
+                    case SignInStatus.LockedOut:
+                        return View("Lockout");
+                    case SignInStatus.RequiresVerification:
+                        return RedirectToAction("SendCode", new { ReturnUrl = returnPath, RememberMe = false });
+                    case SignInStatus.Failure:
+                    default:
+                        ModelState.AddModelError("", "Invalid login attempt.");
+                        return View(model);
+                }
+            }
+
+            ModelState.AddModelError("", "Invalid login attempt.");
+            return View(model);
+        }
+
+        // GET: /Account/Register
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        // POST: /Account/Register
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid && model != null)
+            {
+                var user = new User { UserName = model.Email, Email = model.Email, Surname = model.Surname, FirstName = model.FirstName, Balance = 0m, };
+                var result = UserManager.Create(user, model.Password);
+                if (result.Succeeded)
+                {
+                    UserManager.AddToRole(user.Id, IdentityConstants.UserRole);
+                    SignInManager.SignIn(user, isPersistent: false, rememberBrowser: false);
+                    return RedirectToAction("Index", "Home");
+                }
+
+                AddErrors(result);
+            }
+
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+        // POST: /Account/LogOff
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LogOff()
+        {
+            AuthenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+            return RedirectToAction("Index", "Home");
+        }
+
+        // GET: /Account/ChangePassword
+        [HttpGet]
+        [Authorize(Roles = IdentityConstants.AdminRole+","+IdentityConstants.UserRole)]
+        public ActionResult ChangePassword()
+        {
+            
+            return View();
+        }
+
+        // POST: /Account/ChangePassword
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult ChangePassword(ChangePasswordViewModel model)
+        {
+            if (!ModelState.IsValid || model == null)
+            {
+                return View(model);
+            }
+
+            var result = UserManager.ChangePassword(User.Identity.GetUserId(), model.OldPassword, model.NewPassword);
+            if (result.Succeeded)
+            {
+                var user = UserManager.FindById(User.Identity.GetUserId());
+                if (user != null)
+                {
+                    SignInManager.SignIn(user, isPersistent: false, rememberBrowser: false);
+                }
+
+                return RedirectToAction("Index", "Profile", null);
+            }
+
+            AddErrors(result);
+            return View(model);
+        }
+
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl)
+        {
+            if (Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ src/RepairCompanyManagement.WebUI/Controllers/IdentityBaseController.cs
@@ -0,0 +1,58 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using RepairCompanyManagement.WebUI.Identity;
+
+namespace RepairCompanyManagement.WebUI.Controllers
+{
+    public class IdentityBaseController : Controller
+    {
+        private ApplicationSignInManager _signInManager;
+        private ApplicationUserManager _userManager;
+
+        public IdentityBaseController()
+        {
+        }
+
+        public IdentityBaseController(ApplicationUserManager userManager, ApplicationSignInManager signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        protected ApplicationSignInManager SignInManager
+        {
+            get
+            {
+                return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
+            }
+
+            private set
+            {
+                _signInManager = value;
+            }
+        }
+
+        protected ApplicationUserManager UserManager
+        {
+            get
+            {
+                return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+        protected IAuthenticationManager AuthenticationManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext().Authentication;
+            }
+        }
+    }
+}
