878c9eed0ac7659406d9ed1f3651b070d764068a
Vadzim Zemchonak <37553515+Zemchonak@users.noreply.github.com>
2021-04-22 19:23:18 +0300

    Add OWIN

---
+++ src/RepairCompanyManagement.WebUI/App_Start/StartupOwin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(RepairCompanyManagement.WebUI.App_Start.StartupOwin))]
+
+namespace RepairCompanyManagement.WebUI.App_Start
+{
+    public sealed class StartupOwin
+    {
+        private StartupOwin()
+        {
+        }
+
+        public static void Configuration(IAppBuilder app)
+        {
+            // Enable the application to use a cookie to store information for the signed in user
+            // and to use a cookie to temporarily store information about a user logging in with a third party login provider
+            // Configure the sign in cookie
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account/Login"),
+            });
+            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+
+            // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
+            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+
+            // Enables the application to remember the second login verification factor such as phone or email.
+            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
+            // This is similar to the RememberMe option when you log in.
+            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+        }
+    }
+}
